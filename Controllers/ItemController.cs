421a9ad719ebcb41c992ca41387a4c7164e539ae
Stephen Gacio <sgacio@mail.usf.edu>
2019-09-18 13:25:50 -0400

    added a get on id and a post api call

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using inventory_all_the_things;
 using inventory_all_the_things.Models;
+using System.Collections.Generic;
 
 namespace inventory_all_the_things.Controllers
 {
@@ -10,19 +12,35 @@ namespace inventory_all_the_things.Controllers
   public class ItemController : ControllerBase
   {
     [HttpGet]
-    public ActionResult<IOrderedEnumerable> GetAllItems()
+    public ActionResult<IEnumerable<Item>> GetAllItems()
     {
       var context = new DatabaseContext();
-      var theThing = context.Items.FirstOrDefault(i => i.Id == Id);
-      if (theThing == null)
+      var theThing = context.Items.OrderByDescending(i => i.SKU);
+      return theThing.ToList();
+    }
+
+    [HttpGet("{Id}")]
+    public ActionResult GetOneItem(int Id)
+    {
+      var context = new DatabaseContext();
+      var OneItem = context.Items.FirstOrDefault(i => i.Id == Id);
+      if (OneItem != null)
       {
-        return NotFound();
+        return Ok(OneItem);
       }
       else
       {
-        return theThing;
+        return NotFound();
       }
+    }
 
+    [HttpPost]
+    public ActionResult<Item> CreateItem([FromBody]Item entry)
+    {
+      var context = new DatabaseContext();
+      context.Items.Add(entry);
+      context.SaveChanges();
+      return entry;
     }
   }
 }
